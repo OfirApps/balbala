962efbf08d4d8bb845b6d3ddea656178aa75dc81
OfirApps <73336956+OfirApps@users.noreply.github.com>
2022-06-23 10:53:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,55 +1,21 @@
 const venom = require('venom-bot')
-const venomOptions = require('./venom-options.js')
 
 const TWENTY_MINUTES = 1200000
-let client = null
 
 dateLog('Started index.js')
 	dateLog('Initializing bot')
 	venom
-		//	create bot with options
-		.create(
-		'Pusay',
-		(statusSession, session) => {
-      console.log('Status Session: ', statusSession); //return isLogged || notLogged || browserClose || qrReadSuccess || qrReadFail || autocloseCalled || desconnectedMobile || deleteToken
-      //Create session wss return "serverClose" case server for close
-      console.log('Session name: ', session);
-    },
-	    {
-		    autoClose: 0,
-		    browserArgs: [
-			'--disable-web-security',
-			'--no-sandbox',
-			'--disable-web-security',
-			'--aggressive-cache-discard',
-			'--disable-cache',
-			'--disable-application-cache',
-			'--disable-offline-load-stale-cache',
-			'--disk-cache-size=0',
-			'--disable-background-networking',
-			'--disable-default-apps',
-			'--disable-extensions',
-			'--disable-sync',
-			'--disable-translate',
-			'--hide-scrollbars',
-			'--metrics-recording-only',
-			'--mute-audio',
-			'--no-first-run',
-			'--safebrowsing-disable-auto-update',
-			'--ignore-certificate-errors',
-			'--ignore-ssl-errors',
-			'--ignore-certificate-errors-spki-list',
-		]}
-	
-)
-		// 	start bot
-		.then((client) => startBot(client))
-		// 	catch errors
-		.catch((err) => {
-			dateLog(err)
-		})
+  .create({
+    session: 'pusay', //name of session
+    multidevice: true // for version not multidevice use false.(default: true)
+  })
+  .then((client) => start(client))
+  .catch((erro) => {
+    console.log(erro);
+  });
 
-function startBot(client) {
+
+function start(client) {
 	dateLog('Starting bot')
 
 	//	restart bot every 20 minutos
