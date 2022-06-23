76106820da7b6def58fe720287eb584539bdd4f1
OfirApps <73336956+OfirApps@users.noreply.github.com>
2022-06-23 10:31:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,38 @@ dateLog('Started index.js')
 	dateLog('Initializing bot')
 	venom
 		//	create bot with options
-		.create(venomOptions)
+		.create({
+		'Pusay',
+		autoClose: 0,
+		browserArgs: [
+			'--disable-web-security',
+			'--no-sandbox',
+			'--disable-web-security',
+			'--aggressive-cache-discard',
+			'--disable-cache',
+			'--disable-application-cache',
+			'--disable-offline-load-stale-cache',
+			'--disk-cache-size=0',
+			'--disable-background-networking',
+			'--disable-default-apps',
+			'--disable-extensions',
+			'--disable-sync',
+			'--disable-translate',
+			'--hide-scrollbars',
+			'--metrics-recording-only',
+			'--mute-audio',
+			'--no-first-run',
+			'--safebrowsing-disable-auto-update',
+			'--ignore-certificate-errors',
+			'--ignore-ssl-errors',
+			'--ignore-certificate-errors-spki-list',
+		],
+	(statusSession, session) => {
+      console.log('Status Session: ', statusSession); //return isLogged || notLogged || browserClose || qrReadSuccess || qrReadFail || autocloseCalled || desconnectedMobile || deleteToken
+      //Create session wss return "serverClose" case server for close
+      console.log('Session name: ', session);
+    }
+})
 		// 	start bot
 		.then((client) => startBot(client))
 		// 	catch errors
