cba8f1e3aa8997cf1966f78f6749220766c2f167
OfirApps <73336956+OfirApps@users.noreply.github.com>
2022-08-01 12:37:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
-
+const PORT = process.env.PORT || 5000
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello Express app!')
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('server started');
 });
 
