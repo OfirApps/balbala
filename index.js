3dde9b18e2f3fce2b38b1b668d6f2ff8546af261
OfirApps <73336956+OfirApps@users.noreply.github.com>
2022-08-01 11:34:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,59 +1,75 @@
-const venom = require('venom-bot')
+const qrcode = require('qrcode-terminal');
+stickersMetaData = {name:"שרוך",author:"@שרוך_בוט"}
+const { Client,LocalAuth,MessageMedia,Util} = require('whatsapp-web.js');
+const client = new Client({
+    authStrategy: new LocalAuth(),
+  headless:true,
+  puppeteer: {
+    args: ['--no-sandbox',
+'--disable-setuid-sandbox',
+'--disable-dev-shm-usage',
+'--disable-accelerated-2d-canvas',
+'--no-first-run',
+'--no-zygote',
+'--disable-gpu'
+          ] 
+  }
+});
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
 
-const TWENTY_MINUTES = 1200000
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+mehobadim = ["972585007045@c.us"]
+//https://audiostorage.yhalaommadaitclass.repl.co/extra/kris.mp3 -- Krissed
+client.on('message', async (msg) => {
+content = msg.body
+const chat = await msg.getChat();
+chat.sendSeen()
+console.log("A new message arrived! - " + content + " - By: " + (msg.author || msg.from))
 
-dateLog('Started index.js')
-	dateLog('Initializing bot')
-	venom
-  .create({
-    session: 'pusay', //name of session
-    multidevice: true // for version not multidevice use false.(default: true)
-  })
-  .then((client) => start(client))
-  .catch((erro) => {
-    console.log(erro);
-  });
-
-
-function start(client) {
-	dateLog('Starting bot')
-
-	//	restart bot every 20 minutos
-	//	stops working otherwise
-	setTimeout(() => {
-		//	close bot
-		client.close()
-		dateLog('Closing bot')
-
-		//	init bot again
-		initBot()
-	}, TWENTY_MINUTES)
-
-	client.onMessage((message) => {
-	datelog(message.body)
-    if (message.body === 'Hi' && message.isGroupMsg === false) {
-      client
-        .sendText(message.from, 'Welcome Venom 🕷')
-        .then((result) => {
-          console.log('Result: ', result); //return object success
-        })
-        .catch((erro) => {
-          console.error('Error when sending: ', erro); //return object error
-        });
+if ((msg.author || msg.from) == "972525077784@c.us") {
+  msg.react("❤")
+} else if (mehobadim.includes(msg.author || msg.from)) {
+  msg.react("👍")
+}
+  
+	if(content === 'img') {
+	const media = await MessageMedia.fromUrl('https://via.placeholder.com/350x150.png');
+msg.reply(media,null,{sendMediaAsSticker:true,stickerName:"",stickerAuthor:"@שרוך_בוט"});
+} else if (content == "לבלב יקקה" && ((msg.author || msg.from) == "972525077784@c.us")) {
+    console.log("sudom")
+    msgs = await chat.fetchMessages({limit:1000})
+    msgs = msgs.reverse()
+    console.log(msgs.length)
+    for (let i = 0; i < msgs.length; i++) {
+      setTimeout(
+        function () {
+          msgs[i].react("❤")
+          console.log("❤ a bish")
+        },200+(i*200)
+      )
+      m9a3rwi3tgr = i
     }
-  });
+    q4cj3try9e = new Date()
+      q4cj3try9e = q4cj3try9e.getTime()
+      console.log("The messages will all be reacted in " + new Date(q4cj3try9e+(500+(msgs.length*500))))
+	} else if (content == "הקלטה אחרונה" || content == "הקלטה אחרונה.mp3" || content == "lastrecord" || content == "lastrecord.mp3") {
+const media = await MessageMedia.fromUrl('https://audiostorage.yhalaommadaitclass.repl.co/extra/kris.mp3');
+msg.reply(media,null,{sendAudioAsVoice:true});
+} else if (content == "!פוליגרף") {
+if (msg.hasQuotedMsg == true) {
+  quoted = await msg.getQuotedMessage()
+if (Math.floor(Math.random()*2+1) == 1) {
+quoted.reply("*בדיקת הפוליגרף הסתיימה*\nו--------------------------------ו\nהודעה הזו היא:\n*אמת!*")
+} else {
+quoted.reply("*בדיקת הפוליגרף הסתיימה*\nו--------------------------------ו\nהודעה הזו היא:\n*שקר!*")
+} 
+} else {
+msg.reply("כדי להשתמש ב *!פוליגרף* צריך להגיב איתו להודעה");
 }
-
-
-//
-//	Aux
-//
-
-// Catch ctrl+C
-process.on('SIGINT', function () {
-	client.close()
-})
-
-function dateLog(text) {
-	console.log(new Date(), ' - ', text)
 }
+});
+client.initialize();
